53c532e98ae4807b6b250b22f5241d8ab36cfa4a
a@a.com <a@a.com>
2015-08-18 11:42:38 +0545

    Starting

---
+++ HortonHiveBrowser/HortonHiveBrowser/Controllers/SampleDataController.cs
@@ -0,0 +1,22 @@
+﻿using HortonHiveBrowser.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HortonHiveBrowser.Controllers
+{
+    public class SampleDataController : Controller
+    {
+        //
+        // GET: /SampleData/
+
+        public ActionResult Index()
+        {
+            var dt = new HiveQueryData().GetDataFromHive();
+            return View(dt);
+        }
+
+    }
+}
