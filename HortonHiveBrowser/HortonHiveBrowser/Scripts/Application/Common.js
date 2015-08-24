b01a735a83b058b8ca2045432d95709c407d4128
a@a.com <a@a.com>
2015-08-24 13:42:48 +0545

    Clean up

---
+++ HortonHiveBrowser/HortonHiveBrowser/Scripts/Application/Common.js
@@ -0,0 +1,14 @@
+﻿var Common = (function(a) {
+
+    function pageLoading(status) {
+        if (status == 'true') {
+            $("#loader").css('display', 'block');
+        }
+        else {
+            $("#loader").css('display', 'none');
+        }
+    }
+
+    a.pageLoading = pageLoading;
+    return a;
+})(window.Common || {})
